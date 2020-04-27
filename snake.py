fc331a0ca8f229e7c5176e194836990c03da3d96
rofitasm <rofitasitimusdalifah@gmail.com>
2020-04-28 01:01:53 +0700

    update

--- snake.py
+++ snake.py
@@ -1,208 +1,157 @@
-import math
-import random
+from pygame import display, time, draw, QUIT, init, KEYDOWN, K_a, K_s, K_d, K_w
+from random import randint
 import pygame
-import tkinter as tk
-from tkinter import messagebox
- 
-class cube(object):
-    rows = 20
-    w = 500
-    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
-        self.pos = start
-        self.dirnx = 1
-        self.dirny = 0
-        self.color = color
- 
-       
-    def move(self, dirnx, dirny):
-        self.dirnx = dirnx
-        self.dirny = dirny
-        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
- 
-    def draw(self, surface, eyes=False):
-        dis = self.w // self.rows
-        i = self.pos[0]
-        j = self.pos[1]
- 
-        pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
-        if eyes:
-            centre = dis//2
-            radius = 3
-            circleMiddle = (i*dis+centre-radius,j*dis+8)
-            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
-            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
-            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
-       
- 
- 
- 
-class snake(object):
-    body = []
-    turns = {}
-    def __init__(self, color, pos):
-        self.color = color
-        self.head = cube(pos)
-        self.body.append(self.head)
-        self.dirnx = 0
-        self.dirny = 1
- 
-    def move(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
- 
-            keys = pygame.key.get_pressed()
- 
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    self.dirnx = -1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
-                elif keys[pygame.K_RIGHT]:
-                    self.dirnx = 1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
-                elif keys[pygame.K_UP]:
-                    self.dirnx = 0
-                    self.dirny = -1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
-                elif keys[pygame.K_DOWN]:
-                    self.dirnx = 0
-                    self.dirny = 1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
-        for i, c in enumerate(self.body):
-            p = c.pos[:]
-            if p in self.turns:
-                turn = self.turns[p]
-                c.move(turn[0],turn[1])
-                if i == len(self.body)-1:
-                    self.turns.pop(p)
-            else:
-                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
-                else: c.move(c.dirnx,c.dirny)
-       
- 
-    def reset(self, pos):
-        self.head = cube(pos)
-        self.body = []
-        self.body.append(self.head)
-        self.turns = {}
-        self.dirnx = 0
-        self.dirny = 1
- 
- 
-    def addCube(self):
-        tail = self.body[-1]
-        dx, dy = tail.dirnx, tail.dirny
- 
-        if dx == 1 and dy == 0:
-            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
-        elif dx == -1 and dy == 0:
-            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
-        elif dx == 0 and dy == 1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
-        elif dx == 0 and dy == -1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
- 
-        self.body[-1].dirnx = dx
-        self.body[-1].dirny = dy
-       
- 
-    def draw(self, surface):
-        for i, c in enumerate(self.body):
-            if i ==0:
-                c.draw(surface, True)
-            else:
-                c.draw(surface)
- 
- 
-def drawGrid(w, rows, surface):
-    sizeBtwn = w // rows
- 
-    x = 0
-    y = 0
-    for l in range(rows):
-        x = x + sizeBtwn
-        y = y + sizeBtwn
- 
-        pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
-        pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
-       
- 
-def redrawWindow(surface):
-    global rows, width, s, snack
-    surface.fill((0,0,0))
-    s.draw(surface)
-    snack.draw(surface)
-    drawGrid(width,rows, surface)
-    pygame.display.update()
- 
- 
-def randomSnack(rows, item):
- 
-    positions = item.body
- 
-    while True:
-        x = random.randrange(rows)
-        y = random.randrange(rows)
-        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
-            continue
-        else:
+from numpy import sqrt
+init()
+
+done = False
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+cols = 25
+rows = 25
+
+width = 600
+height = 600
+wr = width/cols
+hr = height/rows
+direction = 1
+
+screen = display.set_mode([width, height])
+display.set_caption("snake_self")
+clock = time.Clock()
+
+
+def getpath(food1, snake1):
+    food1.camefrom = []
+    for s in snake1:
+        s.camefrom = []
+    openset = [snake1[-1]]
+    closedset = []
+    dir_array1 = []
+    while 1:
+        current1 = min(openset, key=lambda x: x.f)
+        openset = [openset[i] for i in range(len(openset)) if not openset[i] == current1]
+        closedset.append(current1)
+        for neighbor in current1.neighbors:
+            if neighbor not in closedset and not neighbor.obstrucle and neighbor not in snake1:
+                tempg = neighbor.g + 1
+                if neighbor in openset:
+                    if tempg < neighbor.g:
+                        neighbor.g = tempg
+                else:
+                    neighbor.g = tempg
+                    openset.append(neighbor)
+                neighbor.h = sqrt((neighbor.x - food1.x) ** 2 + (neighbor.y - food1.y) ** 2)
+                neighbor.f = neighbor.g + neighbor.h
+                neighbor.camefrom = current1
+        if current1 == food1:
             break
-       
-    return (x,y)
- 
- 
-def message_box(subject, content):
-    root = tk.Tk()
-    root.attributes("-topmost", True)
-    root.withdraw()
-    messagebox.showinfo(subject, content)
-    try:
-        root.destroy()
-    except:
-        pass
- 
- 
-def main():
-    global width, rows, s, snack
-    width = 500
-    rows = 20
-    win = pygame.display.set_mode((width, width))
-    s = snake((255,0,0), (10,10))
-    snack = cube(randomSnack(rows, s), color=(0,255,0))
-    flag = True
- 
-    clock = pygame.time.Clock()
-   
-    while flag:
-        pygame.time.delay(50)
-        clock.tick(10)
-        s.move()
-        if s.body[0].pos == snack.pos:
-            s.addCube()
-            snack = cube(randomSnack(rows, s), color=(0,255,0))
- 
-        for x in range(len(s.body)):
-            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
-                print(\'Score: \', len(s.body))
-                message_box(\'You Lost!\', \'Play again...\')
-                s.reset((10,10))
+    while current1.camefrom:
+        if current1.x == current1.camefrom.x and current1.y < current1.camefrom.y:
+            dir_array1.append(2)
+        elif current1.x == current1.camefrom.x and current1.y > current1.camefrom.y:
+            dir_array1.append(0)
+        elif current1.x < current1.camefrom.x and current1.y == current1.camefrom.y:
+            dir_array1.append(3)
+        elif current1.x > current1.camefrom.x and current1.y == current1.camefrom.y:
+            dir_array1.append(1)
+        current1 = current1.camefrom
+    #print(dir_array1)
+    for i in range(rows):
+        for j in range(cols):
+            grid[i][j].camefrom = []
+            grid[i][j].f = 0
+            grid[i][j].h = 0
+            grid[i][j].g = 0
+    return dir_array1
+
+
+class Spot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.f = 0
+        self.g = 0
+        self.h = 0
+        self.neighbors = []
+        self.camefrom = []
+        self.obstrucle = False
+        if randint(1, 101) < 3:
+            self.obstrucle = True
+
+    def show(self, color):
+        draw.rect(screen, color, [self.x*hr+2, self.y*wr+2, hr-4, wr-4])
+
+    def add_neighbors(self):
+        if self.x > 0:
+            self.neighbors.append(grid[self.x - 1][self.y])
+        if self.y > 0:
+            self.neighbors.append(grid[self.x][self.y - 1])
+        if self.x < rows - 1:
+            self.neighbors.append(grid[self.x + 1][self.y])
+        if self.y < cols - 1:
+            self.neighbors.append(grid[self.x][self.y + 1])
+
+
+grid = [[Spot(i, j) for j in range(cols)] for i in range(rows)]
+
+for i in range(rows):
+    for j in range(cols):
+        grid[i][j].add_neighbors()
+
+snake = [grid[round(rows/2)][round(cols/2)]]
+food = grid[randint(0, rows-1)][randint(0, cols-1)]
+current = snake[-1]
+dir_array = getpath(food, snake)
+food_array = [food]
+
+while not done:
+    clock.tick(12)
+    screen.fill(BLACK)
+    direction = dir_array.pop(-1)
+    if direction == 0:    # down
+        snake.append(grid[current.x][current.y + 1])
+    elif direction == 1:  # right
+        snake.append(grid[current.x + 1][current.y])
+    elif direction == 2:  # up
+        snake.append(grid[current.x][current.y - 1])
+    elif direction == 3:  # left
+        snake.append(grid[current.x - 1][current.y])
+    current = snake[-1]
+
+    if current.x == food.x and current.y == food.y:
+        while 1:
+            food = grid[randint(0, rows - 1)][randint(0, cols - 1)]
+            if not (food.obstrucle or food in snake):
                 break
- 
-           
-        redrawWindow(win)
- 
-       
-    pass
- 
- 
- 
-main()
+        food_array.append(food)
+        dir_array = getpath(food, snake)
+    else:
+        snake.pop(0)
+
+    for spot in snake:
+        spot.show(WHITE)
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j].obstrucle:
+                grid[i][j].show(RED)
+
+    food.show(GREEN)
+    snake[-1].show(BLUE)
+    display.flip()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            done = True
+        elif event.type == KEYDOWN:
+            if event.key == K_w and not direction == 0:
+                direction = 2
+            elif event.key == K_a and not direction == 1:
+                direction = 3
+            elif event.key == K_s and not direction == 2:
+                direction = 0
+            elif event.key == K_d and not direction == 3:
+                direction = 1
